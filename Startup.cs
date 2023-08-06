24d5720447d1e344c5dd415497d8dafc256fc98f
ebtechnology.com.au <ebtechnology.com.au@DESKTOP-S9BSTUE>
2023-08-06 14:07:10 +1000

    Add project files.

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Lab_02.Startup))]
+namespace Lab_02
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
